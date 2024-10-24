772b231123700570b2082ca42448d585e584b370
ayush1841 <np842987@gmail.com>
2024-10-24 23:02:58 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,78 @@
+// import java.util.*;
+// public class Condition {
+//     public static void main(String[] args){
+//         Scanner sc =new Scanner(System.in);
+//         int age= sc.nextInt();
+//         if(age>18) {
+//             System.out.println("Adult");
+
+//         } else{
+//             System.out.println("not Adult");
+//         }
+//     }
+
+// }
+// import java.util.*;
+// public class Condition {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         if(a==b){
+//             System.out.println("Equal");
+
+//         } else if(a>b) {
+//             System.out.println("a is greater");
+//         }
+//         else{
+//             System.out.println("a is lessar");
+//         }
+
+//     }
+// }
+// import java.util.*;
+// public class Condition{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int x=sc.nextInt();
+//         if(x%2==0){
+//             System.out.println("Number is even");
+//         } else {
+//             System.out.println("Nmuber is odd");
+//         }
+            
+        
+        
+//     }
+// }
+
+
+                
+
+//         }
+//     }
+// }    
+// import java.util.*;
+
+// public class Condition {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int button = sc.nextInt();
+        
+//         switch (button) {
+//             case 1:
+//                 System.out.println("hello");
+//                 break;
+//             case 2:
+//                 System.out.println("Namaste");
+//                 break;
+//             case 3:
+//                 System.out.println("Bonjour");
+//                 break;
+//             default:
+//                 System.out.println("Invalid Button");
+//         }
+        
+//         sc.close(); // Close the scanner to avoid resource leaks
+//     }
+// }
